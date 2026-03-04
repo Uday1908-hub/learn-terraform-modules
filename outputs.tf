e5d009ec9d27c74d13027e106558cde7fd8e6460
Uday1908-hub <Uday.Kiran.S@gds.ey.com>
2026-03-04 12:09:47 +0530

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,8 +0,0 @@
-output "resource_group_name" {
- description = "The name of the created resource group"
- value       = module.resource_group.rg_name
-}
-output "resource_group_id" {
- description = "The ID of the created resource group"
- value       = module.resource_group.rg_id
-}
